d04fac9396ee102dfd65d848bbfe6bf2b24f1b8b
dahora56 <34323135+dahora56@users.noreply.github.com>
2019-03-01 11:10:56 -0300

    Create main.cpp
    ta com frescura de upar haha

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "pilhaSequencial.h"
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    int opc, valor;
+    Pilha<int> p(10);
+    do
+    {
+        cout<<"Escolha uma opcao:"<<endl;
+        cout<<"1 - Empilha"<<endl;
+        cout<<"2 - Desempilha"<<endl;
+        cout<<"3 - Elemento do Topo"<<endl;
+        cout<<"4 - Exibe a Pilha"<<endl;
+        cout<<"Escolha: ";
+        cin>>opc;
+        switch(opc)
+        {
+        case 1:
+            if(p.pilhaCheia())
+                cout<<"Pilha cheia..."<<endl;
+            else
+            {
+                cout<<"Digite o valor: ";
+                cin>>valor;
+                p.empilha(valor);
+            }
+            break;
+        case 2:
+            if(p.pilhaVazia())
+                cout<<"Pilha vazia..."<<endl;
+            else
+                cout<<"Desempilhado: "<<p.desempilha()<<endl;
+            break;
+        case 3:
+            if(p.pilhaVazia())
+                cout<<"Pilha vazia..."<<endl;
+            else
+                cout<<"Elemento do topo: "<<p.elementoDoTopo()<<endl;
+            break;
+        case 4:
+            if(p.pilhaVazia())
+                cout<<"Pilha vazia..."<<endl;
+            else
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    cout<<p.v[i]<<endl;
+                }
+            }
+            break;
+        case 9:
+            cout<<"Fim da aplicacao."<<endl;
+            break;
+        default:
+            cout<<"Opcao invalida!"<<endl;
+            break;
+        }
+    }while(opc != 9);
+    cout << "Hello world!" << endl;
+    return 0;
+}
